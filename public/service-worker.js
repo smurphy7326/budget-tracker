72a4b04f72553517a9846bc0102bb0eb08731fce
smurphy7326 <smurphy7326@gmail.com>
2021-12-04 17:34:20 -0500

    trying something new

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,24 +4,24 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
     "/",
-    "./index.html",
-    "./js/index.js",
-    "./css/styles.css",
-    "./icons/icon-72x72.png",
-    "./icons/icon-96x96.png",
-    "./icons/icon-128x128.png",
-    "./icons/icon-144x144.png",
-    "./icons/icon-152x152.png",
-    "./icons/icon-192x192.png",
-    "./icons/icon-384x384.png",
-    "./icons/icon-512x512.png"
+    "./public/index.html",
+    "./public/js/index.js",
+    "./public/css/styles.css",
+    "./public/icons/icon-72x72.png",
+    "./public/icons/icon-96x96.png",
+    "./public/icons/icon-128x128.png",
+    "./public/icons/icon-144x144.png",
+    "./public/icons/icon-152x152.png",
+    "./public/icons/icon-192x192.png",
+    "./public/icons/icon-384x384.png",
+    "./public/icons/icon-512x512.png"
 ];
 
 // Install
 
 self.addEventListener('install', function(evt) {
     evt.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
+        caches.open(CACHE_NAME).then(function (cache) {
             console.log('Installing cache: ' + CACHE_NAME);
             return cache.addAll(FILES_TO_CACHE);
         })
@@ -92,4 +92,3 @@ self.addEventListener('fetch', function(evt) {
         })
     );
 });
-
